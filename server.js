7dceef47f4665d5a3d9e6976f532565bb4e51011
JLRoening <jlroening@mail.fhsu.edu>
2022-05-11 19:18:35 -0500

    Should be finished now that fixed
    Turns out .gitignore is very much needed. Otherwise everything should be in working order.

--- server.js
+++ server.js
@@ -10,28 +10,25 @@ const PORT = process.env.PORT || 3000;
 //connect to MongoDB
 connectDB();
 
-//add CORS -available to all
 app.use(cors());
 
-//handle form data (urlencoded data)
+//handles form data (urlencoded data)
 app.use(express.urlencoded({ extended: false}));
 
-//handle json data
+//handles json data
 app.use(express.json());
 
 //serve static files eg. css, images, etc.
 app.use(express.static(path.join(__dirname, '/public')));
 
-//routes
 app.use('/', require('./routes/root'));
 app.use('/states', require('./routes/api/states'));
 
-//404 status code for pages that don't exist
 app.all('*',(req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-//make sure connected to MongoDB before listening for events
+//check to see if connected to MongoDB
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
