7519a27301ff49c01d983cc13dcd4373c65b5119
JLRoening <jlroening@mail.fhsu.edu>
2022-05-11 21:55:52 -0500

    Changes

--- server.js
+++ server.js
@@ -24,8 +24,15 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/states', require('./routes/api/states'));
 
-app.all('*',(req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
 
 //check to see if connected to MongoDB
